a50d2148671a3a1d672a1acbdd6893e90e88425f
aajjuarez <albertoandres.juarez@gmail.com>
2023-12-20 09:31:25 -0300

    Nuevo mapa

--- components/MapaMisiones.vue
+++ components/MapaMisiones.vue
@@ -1,27 +1,199 @@
 <template>
-    <div style="height:100vh; width:100vw">
-      <LMap
-        ref="map"
-        :zoom="zoom"
-        :center="[-27.3671, -55.8961]"
-      >
-        <LTileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-          layer-type="base"
-          name="OpenStreetMap"
-        />
-      </LMap>
-    </div>
-  </template>
+  <div style="height:80vh; width:100v">
+    <l-map ref="map" :zoom="zoom" :center="[-26.52536, -53.8127]">
+      <l-control-layers position="topright" ></l-control-layers>
+      <l-tile-layer
+        url="https://mt3.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+        layer-type="base"
+        name="Google Satellite"
+      ></l-tile-layer>
+      <l-geo-json 
+        :geojson="limites" 
+        :options="options" 
+        :options-style="styleFunctionLimites" 
+        layer-type="overlay" 
+        name="Límites" 
+      />
+      <l-geo-json 
+        :geojson="cuadriculas" 
+        :options="options" 
+        :options-style="styleFunctionCuadriculas" 
+        layer-type="overlay" 
+        name="Cuadrículas" 
+      />
+      <l-geo-json 
+        :geojson="fajas" 
+        :options="options" 
+        :options-style="styleFunctionFajas"  
+        layer-type="overlay" 
+        name="Fajas" 
+      />
+      <l-geo-json 
+        :geojson="alertas" 
+        :options="optionsAlertasRayos" 
+        :options-style="styleFunctionAlertasRayos" 
+        layer-type="overlay" 
+        name="Rayos"
+      />
+      <l-geo-json 
+        :geojson="alertas" 
+        :options="optionsAlertasBajas" 
+        :options-style="styleFunctionAlertasBaja" 
+        layer-type="overlay" 
+        name="Alertas baja probabilidad"
+      />
+      <l-geo-json 
+        :geojson="fotos" 
+        :options="optionsFotos" 
+        :options-style="styleFunctionFotos" 
+        layer-type="overlay" 
+        name="Trabajo en campo"
+      />
+    </l-map>
+  </div>
+</template>
+
+<script>
+
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LGeoJson, LControlLayers, LIcon } from "@vue-leaflet/vue-leaflet";
+
+export default{
+  components: {
+    LMap,
+    LTileLayer,
+    LGeoJson,
+    LControlLayers,
+    LIcon
+  },
+  data() {
+    return {
+      zoom: 12,
+      limites: null,
+      cuadriculas: null,
+      fajas: null,
+      alertas: null,
+      fotos: null,
+      optionsAlerts: '',
+      styleFunction: '',
+      styleFunctionLimites:{
+        color: 'blue',
+        weight: 4,
+        opacity: 0.7,
+        fillOpacity: 0.0,
+        interactive: false
+      },
+      styleFunctionCuadriculas:{
+        color: 'green',
+        weight: 2,
+        opacity: 0.7,
+        fillOpacity: 0.0,
+        interactive: false
+      },
+      styleFunctionFajas:{
+        color: 'red',
+        weight: 2,
+        opacity: 0.7,
+        fillOpacity: 0.0,
+        interactive: false
+      }
+    };
+  },
+  computed: {
+    optionsFotos() {
+      return {
+        pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: new L.Icon({
+                  'iconUrl': 'http://localhost:3000/images/rgs1_nov_23/'+feature.properties.foto ,
+                  'iconSize': [30, 30],
+                  'iconAnchor': [13, 27],
+                  'popupAnchor':  [1, -24]
+                })});
+            },
+        onEachFeature: this.onEachFeatureFotos
+    }
+    },
+    optionsAlertasRayos() {
+      return {
+        filter: function(feature, layer) {
+            if(feature.properties.tipo == 'Rayo') return true;
+        },
+        pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: new L.Icon({
+                  'iconUrl': 'http://localhost:3000/images/icon/alertas-thunder.svg',
+                  'iconSize': [20, 20],
+                  'iconAnchor': [13, 27],
+                  'popupAnchor':  [1, -24]
+                })});
+            },
+        onEachFeature: this.onEachFeatureFunction
+    }
+    },
+    optionsAlertasBajas() {
+      return {
+        filter: function(feature, layer) {
+            if(feature.properties.tipo == 'Con probabilidad baja') return true;
+        },
+        pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: new L.Icon({
+                  'iconUrl': 'http://localhost:3000/images/icon/alertas-fire-baja.svg',
+                  'iconSize': [20, 20],
+                  'iconAnchor': [13, 27],
+                  'popupAnchor':  [1, -24]
+                })});
+            },
+        onEachFeature: this.onEachFeatureFunction
+    }
+    },
+    onEachFeatureFunction() {
+      return (feature, layer) => {
+        layer.bindTooltip(
+          'Fecha: '+feature.properties.acq_date,
+          { permanent: false, sticky: true }
+        );
+      };
+    },
+    onEachFeatureFotos() {
+      return (feature, layer) => {
+        if(feature.properties.foto) {
+          layer.bindPopup(
+          '<img src="http://localhost:3000/images/rgs1_nov_23/'+feature.properties.foto+'" />',
+          { permanent: false, sticky: true }
+        );
+        }
+      };
+    }
+  },
+  async created() {
+    const response = await fetch('http://localhost:3000/capas/limites.geojson')
+    const data = await response.json();
+    this.limites = data;
+
+    const responseC = await fetch('http://localhost:3000/capas/cuadriculas.geojson')
+    const dataC = await responseC.json();
+    this.cuadriculas = dataC;
+
+    const responseF = await fetch('http://localhost:3000/capas/reforestacion_fajas.geojson')
+    const dataF = await responseF.json();
+    this.fajas = dataF;
+
+    const responseFo = await fetch('http://localhost:3000/capas/fotos.geojson')
+    const dataFo = await responseFo.json();
+    this.fotos = dataFo;
+
+    const responseA = await fetch('https://script.google.com/macros/s/AKfycbydNCzG37SZ88WEZIoikFGoZTqVNA02CHLbuZtxTO_S3mj-6jJS7he3v3q38-lZ5ghO/exec')
+    const dataA = await responseA.json();
+    this.alertas = dataA;
+
+  }
+};
+</script>
   
-  <script setup>
-  import { ref } from 'vue'
-  const zoom = ref(12)
-  </script>
-  
-  <style>
+<style>
   body {
     margin: 0;
   }
-  </style>
+  .leaflet-popup-content {
+    width: 250px;
+  }
+</style>
