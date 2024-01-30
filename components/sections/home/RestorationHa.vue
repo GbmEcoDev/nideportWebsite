c8ece3e7304292b3b2eb00256f48b4c0f6daeacd
Ariel <arielorobles@gmail.com>
2024-01-30 14:37:28 -0300

    act contador

---
+++ components/sections/home/RestorationHa.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="contador">
+        <div class="color_fondo" :style="{ height: colorHeight }"></div>
+        <div class="numero xs:text-4xl xs:leading-7 lg:text-6xl">{{ cantidad.toLocaleString() }} HECTAREAS </div>
+    </div>
+</template>
+
+<script setup>
+import { ref,onMounted, onBeforeUnmount, onActivated } from "vue"
+const colorHeight = ref('0%');
+const cantidad = ref(0);
+let tiempo;
+
+const iniciarContador = () => {
+tiempo = setInterval(() => {
+    cantidad.value += 354321;
+    colorHeight.value = `${(cantidad.value / 45000000) * 95}%`;
+
+        if (cantidad.value >= 45000000) {
+        cantidad.value = 45000000;
+        clearInterval(tiempo);
+        }
+    }, 50);
+};
+onMounted(() => {
+
+    iniciarContador();
+
+});
+
+onActivated(() => {
+    // Reinicia el contador cuando el componente es activado nuevamente
+    cantidad.value = 0;
+    colorHeight.value = '0%';
+    iniciarContador();
+  });
+
+onBeforeUnmount(() => {
+  clearInterval(tiempo);
+});
+</script>
+
+<style scoped>
+.contador {
+  transform: rotate(180deg);
+  margin: 0px;
+  position: relative;
+  height:4.1em;
+}
+
+.numero {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+  font-family: sans-serif;
+  color: #1c9577;
+  position: absolute;
+  transform: rotate(-180deg);
+  mix-blend-mode: lighten;
+    background-color: #e7f8f6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.color_fondo {
+  transition: 0.2s;
+  transform-origin: bottom;
+  position: absolute;
+  display: block;
+  height: 0%;
+  width: 100%;
+  height:3.9em;
+  background-color: rgba(10, 190, 46, 0.796);
+}
+</style>
